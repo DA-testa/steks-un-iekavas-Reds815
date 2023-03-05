3bb25d05509825e96a6f475884cb1a297c99d30b
Reds815 <Reds-Ricards.Polis@edu.rtu.lv>
2023-03-05 13:31:17 +0200

    update

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
 def are_matching2(left, right):
     if (left + right) in ["()", "[]", "{}"]:
         return True # kad izveidojās atverošās/aizverošās iekavu pārītis
@@ -16,35 +15,37 @@ def are_matching2(left, right):
         return False # kad neizveidojās atverošās/aizverošās iekavu pārītis
     
 
+
 def find_mismatch(text):
+    print("tests2")
     opening_brackets_stack = []
-    #for i, next in enumerate(text):
-    for i in range(0, len(text)): 
-          #if next in "([{":
-          if text[i] in "([{":
-               # Process opening bracket, write your code here
-               # opening_brackets_stack.append(Bracket(next,i))
-               opening_brackets_stack.append(text[i])
-        
-          if text[i] in ")]}":
-               # Process closing bracket, write your code here
-               if len(opening_brackets_stack) == 0: # ja steks ar iekavaam ir tuks
-                      return i+1           # steks ir tukšs
-               else:  
-                      if (are_matching2(opening_brackets_stack.pop(),text[i]) == False):
-                          return i+1       # kluda, jo nesakrit iekavas
-    if len(opening_brackets_stack) == 0:   # vai nepalika vēl stekā atverošās iekavas
-        return ("Success")                 # visas iekavas iztērētas 
-    else:
-        return i+1                         # kluda, jo nesakrit iekavas  
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(next)
+            pass
+
+        if next in ")]}":
+            # Process closing bracket, write your code here
+            if len(opening_brackets_stack) == 0:
+                print (i)
+            pass
+            if len(opening_brackets_stack) != 0:
+                if (are_matching2(opening_brackets_stack.pop(), next) == False):
+                    print(i)
+                pass
+            pass
+        pass
+
 
 
 def main():
-    x = input("Ievadiet pārbaudāmo simbolu virkni:")
-    print()
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    print(mismatch)
 
+print ("tests")
+
+#if __name__ == "__main__":
 main()
+pass
